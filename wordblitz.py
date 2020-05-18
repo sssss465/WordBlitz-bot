06ba87e082666c4c1c27a99a9777bc2e98936285
Andrew <sssss465@gmail.com>
2020-05-17 18:16:37 -0700

    cleaned up printing

--- wordblitz.py
+++ wordblitz.py
@@ -151,7 +151,7 @@ def draw(coords: List, speed=0.0) -> None:
     time.sleep(1)
     for w, cd in coords:
         off = grid_to_pix[cd[0]]
-        print('drawing', w)
+        print(cf.green('drawing {w}\r'))
         pyautogui.moveTo(off[0], off[1], duration=speed)
         pyautogui.mouseDown()
         for c in cd[1:]:
